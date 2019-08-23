763397b8dd67e677f06d35230cda6e31daae394a
ROMAN BEREZDETSKYI <berezdecky98@gmail.com>
2019-08-23 14:31:44 +0300

    axios imported

---
+++ src/components/Calculator.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>{{result}}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      result: null
+    }
+  },
+  methods: {
+
+  },
+  async created() {
+    try {
+      const response = await this.$http.post("/monthly-installment", {
+        amount: 10000,
+        duration: 5,
+      });
+      console.log(response);
+      this.result = response.data
+    } catch(err) {
+      console.log(err);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+import axios from 'axios';
 
-Vue.config.productionTip = false
+const base = axios.create({
+  baseURL: 'http://localhost:3300'
+})
+
+Vue.config.productionTip = false;
+Vue.prototype.$http = base;
 
 new Vue({
-  router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
